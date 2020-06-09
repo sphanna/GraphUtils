d0f2700cb26a0eee3942670b86365c4a72389698
sphanna <30737060+sphanna@users.noreply.github.com>
2020-06-09 08:30:35 -0400

    Create HavelHakimi.jl

---
+++ src/HavelHakimi.jl
@@ -0,0 +1,21 @@
+#Havel-Hakimi Algorithm
+function isGraphical(seq)
+    Seq = copy(seq)
+    graphical = true
+    subSeqs = []
+    push!(subSeqs,copy(Seq))
+    sort!(Seq,rev=true)
+    while sum(Seq) > 0
+        max = popfirst!(Seq)
+        if max > length(Seq)
+            graphical = false; break;
+        end
+        Seq[1:max] = Seq[1:max].-1
+        sort!(Seq,rev=true)
+        push!(subSeqs,copy(Seq))
+        if any(x->x<0,Seq) #If seq contains a negative value
+            graphical = false; break;
+        end
+    end
+    return (graphical,subSeqs)
+end
